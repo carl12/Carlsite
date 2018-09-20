19efb80b5ce940fbcb22f0716974c91340b27245
carl12 <carl.rodriguez13@gmail.com>
2018-09-20 16:52:46 -0700

    Update api request with hidden key

--- asciiC.py
+++ asciiC.py
@@ -1,27 +1,37 @@
-
+import logging
 import urllib2
 # from urllib.request import urlopen
+import json
 from xml.dom import minidom
 from google.appengine.ext import db
 from handlers import Handler
+from lib import apiKey
 
 
-IP_URL ="http://freegeoip.net/xml/"
+IP_URL ="http://api.ipstack.com/"
+keyStr = "?access_key="+ apiKey.ipStack_key
+
 def get_coords(ip):
-    # ip = '50.193.21.147'
-    url = IP_URL + ip
+    # ip = '50.193.21.147' # test ip address
+    url = IP_URL + ip + keyStr
     content = None
     try:
         content = urllib2.urlopen(url).read()
-    except URLError: 
+    except urllib2.URLError: 
+        logging.error('urlerror occurred')
+        logging.info(url)
         return
     if content:
         try:
-            lat = minidom.parseString(content).getElementsByTagName("Latitude")[0].childNodes[0].nodeValue
-            lon = minidom.parseString(content).getElementsByTagName("Longitude")[0].childNodes[0].nodeValue
+            content = json.loads(content)
+            lat = content['latitude']
+            lon = content['longitude']
             return db.GeoPt(lat,lon)
         except:
+            logging.error('some sort of error getting lat and long')
             return
+    else:
+        logging.info('no content')
 
 GMAPS_URL = 'http://maps.google.com/maps/api/staticmap?size=380x263&sensor=false&'
 def gmaps_img(points):
@@ -52,7 +62,7 @@ class AsciiPage(Handler):
                     img_url=img_url)
 
     def get(self):
-        # self.write(self.request.remote_addr)
+        self.write(self.request.remote_addr)
         self.write(repr(get_coords(self.request.remote_addr)))
         self.render_front()
 
@@ -62,6 +72,7 @@ class AsciiPage(Handler):
         if title and art:
             a = Art(title=title, art=art)
             coords = get_coords(self.request.remote_addr)
+            coords = None
             if coords: 
                 a.coords = coords
             a.put()
